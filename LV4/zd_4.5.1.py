916441b29c5acdbd97a97c6be9888be30bf322aa
mhasenay <mhasenay@gmail.com>
2026-03-26 13:06:13 +0100

    Editing LV4 tasks and adding pdf

--- LV4/zd_4.5.1.py
+++ LV4/zd_4.5.1.py
@@ -6,68 +6,74 @@ from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 import sklearn . linear_model as lm
 from sklearn . metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error, r2_score
 
-df = pd.read_csv("data_C02_emission.csv")
+df = pd.read_csv("../LV3/data_C02_emission.csv")
 
+#a) Odaberite željene numeričke veličine s listom naziva stupaca. Podijeliti podatke
+#na skup za učenje i skup za testiranje u omjeru 70%-30%
 input = ['Engine Size (L)', 'Cylinders', 'Fuel Consumption City (L/100km)', 'Fuel Consumption Hwy (L/100km)', 
              'Fuel Consumption Comb (L/100km)', 'Fuel Consumption Comb (mpg)']
 output = ['CO2 Emissions (g/km)']
 
 X = df[input]
 y = df[output]
-print(X)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-
-
+#print(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+'''
+#b) Napravit dijagrame raspršenja i prikazati ovisnost emisije CO2 plinova o
+#ostalim numeričkim veličinama
 for i in range(len(input)):
     plt.figure()
     
-    plt.scatter(X_train.iloc[:, i], y_train, color="blue", alpha=0.4, label="Učenje")
-    plt.scatter(X_test.iloc[:, i],  y_test,  color="red",  alpha=0.4, label="Test")
+    plt.scatter(X_train.iloc[:, i], y_train, color="blue", alpha=0.5, label="Učenje")
+    plt.scatter(X_test.iloc[:, i],  y_test,  color="red",  alpha=0.5, label="Test")
     
     plt.xlabel(X_train.columns[i])
     plt.ylabel(output)
-    plt.title(f"CO2 emisija vs {X_train.columns[i]}")
+    plt.title(f"CO2 Emission (g/km) vs {X_train.columns[i]}")
     plt.legend()
     
     plt.show()
-
-
+'''
+#c) Izvršiti standardizaciju ulaznih veličina na skupu za učenje
+#prikazati histograme ulaznih veličina prije i nakon skaliranja
 sc = MinMaxScaler()
 X_train_n = sc.fit_transform ( X_train )
 X_test_n = sc.transform ( X_test )
 
-
+'''
 plt.figure()
 for i in range(len(input)):
     plt.subplot(1, 2, 1)
-    plt.hist(X_train.iloc[:, i], bins=30, color="blue")
+    plt.hist(X_train.iloc[:, i], bins=20, color="blue")
     plt.title("Prije skaliranja")
 
     plt.subplot(1, 2, 2)
-    plt.hist(X_train_n[:, i], bins=30, color="orange")
+    plt.hist(X_train_n[:, i], bins=20, color="magenta")
     plt.title("Nakon skaliranja")
     plt.show()
+'''
+#d) Izgradite linearni regresijski model i ispisati u terminal dobivene parametre modela
+linearModel = lm.LinearRegression()
+linearModel.fit(X_train_n , y_train)
+print(f"Parametri modela: {linearModel.coef_}")
 
-ohe = OneHotEncoder ()
-X_encoded = ohe.fit_transform(df[['Fuel Type']]).toarray()
-linearModel = lm. LinearRegression ()
-linearModel.fit( X_train_n , y_train )
-
-
-y_test_p = linearModel . predict ( X_test_n )
-
+#e) Izvršite procjenu izlazne veličine na temelju ulaznih veličina skupa za testiranje
+#prikazati pomoću dijagrama raspršenja odnos između stvarnih vrijednosti izlaza i procjene
+y_test_p = linearModel.predict(X_test_n)
 
 plt.figure()
-plt.scatter(y_test, y_test_p, alpha=0.5)
+plt.scatter(y_test, y_test_p, alpha=0.5, c="green")
 plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()])
 plt.xlabel("Stvarne vrijednosti")
 plt.ylabel("Procijenjene vrijednosti")
 plt.title("Stvarno vs Procijenjeno")
 plt.show()
 
+#f) Izvršite vrednovanje modela na način da se izračunaju vrijednosti regresijskih metrika
+#na skupu podataka za testiranje
 print("\nMetrike na testnom skupu:")
-print(f"  MAE  = {mean_absolute_error(y_test, y_test_p):.4f}")
-print(f"  MSE  = {mean_squared_error(y_test, y_test_p):.4f}")
-print(f"  RMSE = {np.sqrt(mean_squared_error(y_test, y_test_p)):.4f}")
-print(f"  R2   = {r2_score(y_test, y_test_p):.4f}")
-print(f"  MAPE   = {mean_absolute_percentage_error(y_test, y_test_p):.4f}")
+print(f"MAE = {mean_absolute_error(y_test, y_test_p):.4f}")
+print(f"MSE = {mean_squared_error(y_test, y_test_p):.4f}")
+print(f"RMSE = {np.sqrt(mean_squared_error(y_test, y_test_p)):.4f}")
+print(f"R2 = {r2_score(y_test, y_test_p):.4f}")
+print(f"MAPE = {mean_absolute_percentage_error(y_test, y_test_p):.4f}")
--- LV4/zd_4.5.2.py
+++ LV4/zd_4.5.2.py
@@ -6,32 +6,43 @@ from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 import sklearn.linear_model as lm
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_absolute_percentage_error, max_error
 
-df = pd.read_csv("data_C02_emission.csv")
+df = pd.read_csv("../LV3/data_C02_emission.csv")
 
 input = ["Engine Size (L)", "Cylinders", "Fuel Consumption Comb (L/100km)", "Fuel Consumption City (L/100km)", "Fuel Consumption Hwy (L/100km)", "Fuel Type"]
 output = "CO2 Emissions (g/km)"
 
+#Na temelju prošlog zadatka izraditi model koji koristi Fuel Type kao ulaznu veličinu
+#Koristiti OneHotEncoder
+#Ne skalirati ulazne podatke
 X = df[input]
 y = df[output]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+ohe = OneHotEncoder ()
+X_encoded_train = ohe.fit_transform(X_train[['Fuel Type']]).toarray()
+X_encoded_test = ohe.fit_transform(X_test[['Fuel Type']]).toarray()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-sc = MinMaxScaler()
-X_train_n = sc.fit_transform ( X_train )
-X_test_n = sc.transform ( X_test )
+linearModel = lm.LinearRegression ()
+linearModel.fit(X_encoded_train, y_train )
 
+y_test_p = linearModel.predict(X_encoded_test)
 
-
-
-linearModel = lm. LinearRegression ()
-linearModel . fit ( X_train_n , y_train )
-
-
-y_test_p = linearModel . predict ( X_test_n )
-
-
+'''
+print("\nMetrike na testnom skupu:")
+print(f"MAE  = {mean_absolute_error(y_test, y_test_p):.4f}")
+print(f"MSE  = {mean_squared_error(y_test, y_test_p):.4f}")
+print(f"RMSE = {np.sqrt(mean_squared_error(y_test, y_test_p)):.4f}")
+print(f"R2   = {r2_score(y_test, y_test_p):.4f}")
+print(f"MAPE = {mean_absolute_percentage_error(y_test, y_test_p):.4f}")
+'''
 print("Maksimalna pogreška:", max_error(y_test, y_test_p))
 
+errors = np.abs(y_test.values - y_test_p)
+max_error_id = np.argmax(errors)
 
+max_error_model = df.iloc[max_error_id, 1]
+print(f"Model vozila s najvećom pogreškom: {max_error_model}")
+
+'''
 plt.figure()
 plt.scatter(y_test, y_test_p, alpha=0.5)
 plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], "k--")
@@ -39,11 +50,4 @@ plt.xlabel("Stvarne vrijednosti")
 plt.ylabel("Procijenjene vrijednosti")
 plt.title("Stvarno vs Procijenjeno")
 plt.show()
-
-
-print("\nMetrike na testnom skupu:")
-print(f"MAE  = {mean_absolute_error(y_test, y_test_p):.4f}")
-print(f"MSE  = {mean_squared_error(y_test, y_test_p):.4f}")
-print(f"RMSE = {np.sqrt(mean_squared_error(y_test, y_test_p)):.4f}")
-print(f"R2   = {r2_score(y_test, y_test_p):.4f}")
-print(f"MAPE = {mean_absolute_percentage_error(y_test, y_test_p):.4f}")
+'''
